3bb3f4e3b37df39489b327d370320f622af12322
Daeyoon <yoonda5898@gmail.com>
2021-09-07 21:55:21 +0900

    Implemented post deleting and editing

--- src/main/java/io/github/yoon6/basiccrud/controller/PostController.java
+++ src/main/java/io/github/yoon6/basiccrud/controller/PostController.java
@@ -4,9 +4,11 @@ import io.github.yoon6.basiccrud.model.entity.PostEntity;
 import io.github.yoon6.basiccrud.model.repository.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class PostController {
@@ -36,5 +38,18 @@ public class PostController {
         model.addAttribute("post", post);
 
         return "detail";
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        postRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PutMapping("/post/{id}")
+    public String edit(@PathVariable("id") Long id, PostEntity postEntity) {
+        postRepository.save(postEntity);
+
+        return "redirect:/post/" + id;
     }
 }
